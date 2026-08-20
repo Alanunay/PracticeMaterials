f02dc05a4a95c93bf619ab45999d446f3b65cf08
Yeuur <ajcalidguid12345@gmail.com>
2026-08-20 15:19:55 +0800

    feat: remove word "User"

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,7 +3,7 @@ import { UserInfo } from "./UserInfo";
 export const UserCard=(props) =>{
     return(
         <>
-             <h2>User Details</h2>
+             <h2>Details</h2>
             <UserInfo {...props}/>
         </>
     );
